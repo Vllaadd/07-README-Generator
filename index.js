b031a4e92605be730ca8912f20e237edd58c813f
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-09-05 07:29:45 -0400

    changing the approach

--- index.js
+++ index.js
@@ -1,59 +1,47 @@
-const prompt = require('inquirer').createPromptModule() //inquirer should ease the process of providing error feedback, asking questions, parsing input, validating answers, managing hierarchical prompts.
-const fs = require('fs') //file system is a mechanism that controls how data is stored, accessed, and managed. Using fs module, we can perform read, write, delete and many more operations.
-
-const api = require('./utils/api.js') //this line is only because we decided to put api in a separate file 
+const inquirer = require('inquirer')
+const fs = require('fs') 
+const axios = require('axios')
 const generateMarkdown = require('./utils/generateMarkdown.js')
+const api = require('./utils/api.js') 
 
+const gitInput = {}
 
-const writeToFile = (fileName, data) => {
-    fs.writeFile(fileName + '.md', data, error => error ? console.error(error) : console.log(`${fileName + '.md'} generated!`))
-}
-
-const init = async _ => { //this is the main function 
-    let rmObject = {}
-    do {
-        const { rmUser, rmRepo } = await prompt([
+        const gitQuestions = [
             {
                 type: 'input',
-                name: 'rmUser',
+                name: 'gitUserName',
                 message: 'What is your GitHub user name?'
-            },
-            {
+            },{
                 type: 'input',
-                name: 'rmRepo',
+                name: 'gitRepoName',
                 message: 'What is your repository name?'
+            },{
+                type: 'input',
+                name: 'projectDescript',
+                message: 'What ist the project description?'
+            },{
+                type: 'input',
+                name: 'instructions',
+                message: 'What are the installation instructions?'
+            },{
+                type: 'input',
+                name: 'use',
+                message: 'What is the usage description?'
+            },{
+                type: 'input',
+                name: 'contributors',
+                message: 'Who are the contributors?'
+            },{
+                type: 'input',
+                name: 'questions',
+                message: 'Do you have any questions?'
             }
-        ])
-        rmObject = await api.getUser(rmUser, rmRepo)
-        if(!rmObject){
-            console.error('Repo not found!')
-        }else{
-            console.log(`${rmObject.fullName} found!`)
-        }
-    }while (!rmObject)
-    Object.assign(rmObject, await prompt([
-        {
-            type: 'input',
-            name: 'inst',
-            message: 'What are the installation instructions?'
-        },
-        {
-            type: 'input',
-            name: 'use',
-            message: 'What is the usage description?'
-        },
-        {
-            type: 'input',
-            name: 'con',
-            message: 'Who are the contributors?'
-        },
-        {
-            type: 'input',
-            name: 'qs',
-            message: 'Any questions?'
-        }
-    ]))
-    writeToFile(rmObject.title, await generateMarkdown(rmObject))
-}
+        ];
 
-init()
+        inquirer
+            .prompt(gitQuestions)
+            .them((userInput) => {
+                const queryUrl = () => {
+                    var queryUrl = 
+                }
+            })
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,14 @@
 const generateMarkdown = data => {
-    return `<a href="${data.link}" style="float:rigth"><img src="${data.avatar}" alt="${data.name}" title="${data.name}" width="120" height="120"></a>
+    return `
+    <a href="${data.link}" style="float:rigth"><img src="${data.avatar}" alt="${data.name}" title="${data.name}" width="120" height="120"></a>
     # ${data.title.toUpperCase()}
     ![License: ${(data.lic) ? data.lic : 'None'}](https://img.shields.io/badge/License-${(data.lic) ? data.lic : 'None'}-brightgreen)
     
     _Repo by ${data.name}_
     _${data.desc}_
-    ___
+
+    ---
+    
     __Installation:___
     ${data.inst}
     
