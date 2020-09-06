0d10315b1aba7f6d220392a68762328ff05b8e58
Vladimir Zizic <vlladdiimiir@gmail.com>
2020-09-06 00:32:12 -0400

    working on generateMarkdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,28 +1,12 @@
-const generateMarkdown = data => {
+function generateMarkdown(userAnswers, data){
     return `
-    <a href="${data.link}" style="float:rigth"><img src="${data.avatar}" alt="${data.name}" title="${data.name}" width="120" height="120"></a>
-    # ${data.title.toUpperCase()}
-    ![License: ${(data.lic) ? data.lic : 'None'}](https://img.shields.io/badge/License-${(data.lic) ? data.lic : 'None'}-brightgreen)
-    
-    _Repo by ${data.name}_
-    _${data.desc}_
+${userAnswers, gitRepoName}
+${userAnswers.projectDescription}.
 
-    ---
-    
-    __Installation:___
-    ${data.inst}
-    
-    ___Usage:___
-    ${data.use}
-    
-    ___Contributors:___
-    ${data.con}
-    
-    ___Tests:___
-    ${data.test}
-    
-    ___Questions:___
-    ${data.qs}
-    `
+${userAnswers.projectInstallation}
+
+${userAnswers.use}
+
+`;
 }
-module.exports = generateMarkdown
+module.exports = generateMarkdown;
